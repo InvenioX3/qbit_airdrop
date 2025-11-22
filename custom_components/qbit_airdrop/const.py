eefdc4e4c59e949c27dae940e8a8a51fd96a02a7
Joseph Brandenburg <inveniofirmamentum@gmail.com>
2025-11-22 01:13:24 -0800

    a

--- custom_components/qbit_airdrop/const.py
+++ custom_components/qbit_airdrop/const.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 DOMAIN = "qbit_airdrop"
 
-# Connection (non-auth)
+# Connection
 CONF_HOST = "host"
 CONF_PORT = "port"
-
-# Default base save path, e.g. //My_NAS/Media/   (optional; can be blank)
 CONF_BASE_PATH = "base_path"
+
+# Optional WebUI authentication
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
