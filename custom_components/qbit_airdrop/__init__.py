12509713e836a915a3000d93f76941b18d66305a
Joseph Brandenburg <inveniofirmamentum@gmail.com>
2026-06-30 18:31:21 -0700

    Update __init__.py

--- custom_components/qbit_airdrop/__init__.py
+++ custom_components/qbit_airdrop/__init__.py
@@ -177,27 +177,23 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                     timeout=10,
                 )
 
-                    folder_source = None
+            folder_source = None
 
-                if best:
-                    folder_source = best["name"]
+            if best:
+                folder_source = best["name"]
 
-                if best and episode_name:
-                    ...
-                    renameFile()
+            if season and folder_source and "/" in folder_source:
+                root_folder = folder_source.split("/", 1)[0]
 
-                if season and folder_source and "/" in folder_source:
-                    root_folder = folder_source.split("/", 1)[0]
-
-                    await session.post(
-                        f"{base}/api/v2/torrents/renameFolder",
-                        data={
-                            "hash": torrent_hash,
-                            "oldPath": root_folder,
-                            "newPath": season,
-                        },
-                        timeout=10,
-                    )
+                await session.post(
+                    f"{base}/api/v2/torrents/renameFolder",
+                    data={
+                        "hash": torrent_hash,
+                        "oldPath": root_folder,
+                        "newPath": season,
+                    },
+                    timeout=10,
+                )
 
         except Exception:
             pass
